c04beacee269794fdae86e7271e1893aa15f86ff
Venkata Kanishk Payyavala <venkatakanishk244@gmail.com>
2026-03-07 16:31:03 +0530

    changed something

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ resource "aws_security_group" "main" {
     to_port          = 0
     protocol         = "-1"
     cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
+
   }
 
     tags = merge( var.sg_tags,
